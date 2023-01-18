1415743ee43a3504766a3adfd36a4ba596e38ef6
Karthik Venkatesh <kvenkate@tcd.ie>
2023-01-18 17:01:56 +0300

    graphcache

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,7 +1,17 @@
 import { ThemeProvider, CSSReset, ColorModeProvider } from '@chakra-ui/core'
-import {createClient, dedupExchange, fetchExchange, Provider } from 'urql'
-import { cacheExchange } from '@urql/exchange-graphcache';
+import {createClient, dedupExchange, fetchExchange, Provider, Query } from 'urql'
+import { cacheExchange, Cache, QueryInput } from '@urql/exchange-graphcache';
 import theme from '../theme'
+import { LoginMutation, MeDocument, MeQuery, RegisterMutation } from '../gql/graphql';
+
+function betterUpdateQuery<Result, Query>(
+    cache: Cache, 
+    qi: QueryInput,
+    result: any, 
+    fn: (r: Result, q: Query) => Query
+  ){
+    return cache.updateQuery(qi, (data)=> fn(result, data as any) as any);
+} 
 
 const client = createClient(
   {
@@ -9,8 +19,46 @@ const client = createClient(
     fetchOptions: {
       credentials:"include",
     },
-    exchanges: [dedupExchange, cacheExchange({}), fetchExchange]
-    
+    exchanges: [dedupExchange, cacheExchange({
+      updates:{
+        Mutation: {
+          login: (_result, args, cache, info) => {
+            betterUpdateQuery<LoginMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.login.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.login.user,
+                  };
+                }
+              }
+            );
+          },
+          register: (_result, args, cache, info) => {
+            betterUpdateQuery<RegisterMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.register.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.register.user,
+                  };
+                }
+              }
+            );
+          },
+        },
+      },
+    }),
+    fetchExchange,
+  ],
 });
 
 function MyApp({ Component, pageProps }) {
